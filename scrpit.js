71cc2a79b634d41999820e69de80819859a767b0
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2023-11-16 10:31:04 -0300

    atualizado innerHtml para controle de textos da aplicacao

--- scrpit.js
+++ scrpit.js
@@ -4,12 +4,28 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 
 function alterarContexto(valor) {
 
     html.setAttribute('data-contexto', `${valor}`)
     banner.setAttribute('src', `imagens/${valor}.png`)
+
+    if (valor === "foco") {
+        titulo.innerHTML = `Otimize sua produtividade,<br>
+    <strong class="app__title-strong">mergulhe no que importa.</strong>`
+    } else if (valor === "descanso-curto") {
+
+        titulo.innerHTML = `Que tal dar uma respirada?
+ <strong class="app__title-strong">faça uma pausa</strong>`
+    } else if (valor === "descanso-longo") {
+
+        titulo.innerHTML = `Hora de voltar da superficie
+ <strong class="app__title-strong">faça uma pausa longa</strong>`
+    }
+
+
 }
 
 focoBt.addEventListener('click', () => {
